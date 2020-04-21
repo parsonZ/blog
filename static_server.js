646d2e032d8a2d72e216219ae3c06f0cdc08105a
pangzong <v_zongpang@tencent.com>
2020-04-22 00:02:36 +0800

    'fix'

--- static_server.js
+++ static_server.js
@@ -5,16 +5,15 @@ const port = 8000
 
 app.all('*', function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,ts');
     res.header('Access-Control-Allow-Methods', '*');
     res.header('Content-Type', 'application/json;charset=utf-8');
     next();
 })
 
-app.use('/images', express.static('static'))
+app.use('/static', express.static('static'))
 
 app.use('/', (req, res) => res.send({ 
-    isSuccess: true,
     msg: '我是来自8000上的数据'
 }))
 
